c1b367e0e6bf7299e9aca0b05551ea04f1ba4f8c
Ruheni Alex <aleruheni@outlook.com>
2018-12-26 23:42:25 +0300

    bookinstanceController created with dummy handler functions

---
+++ controllers/bookinstanceController.js
@@ -0,0 +1,41 @@
+var BookInstance = require('../models/bookinstance');
+
+// Display list of all BookInstances
+exports.bookinstance_list = (req, res) => {
+    res.send('NOT IMPLEMENTED: BookInstance list');
+};
+
+//Display detail page for a specific BookInstance
+exports.bookinstance_detail = (req, res) => {
+    res.send(`NOT IMPLEMENTED: BookInstance detail: ${req.params.id}`);
+};
+
+// Display BookInstance create form on GET
+exports.bookinstance_create_get = (req, res) => {
+    res.send('NOT IMPLEMENTED: BookInstance create GET');
+};
+
+// Handle BookInstance create on POST
+exports.bookinstance_create_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: BookInstance create POST');
+};
+
+// Display BookInstance delete on GET
+exports.bookinstance_delete_get = (req, res) => {
+    res.send('NOT IMPLEMENTED: BookInstance delete on GET');
+};
+
+// Handle BookInstance delete on POST
+exports.bookinstance_delete_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: BookInstance delete on POST');
+};
+
+// Display BookInstance update on GET
+exports.bookinstance_update_get = (req, res) => {
+    res.send('NOT IMPLEMENTED: BookInstance update on GET');
+};
+
+// Handle BookInstance update on POST
+exports.bookinstance_update_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: BookInstance update on POST');
+};
