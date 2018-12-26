af9a7dd126d2eeea4a4f7281ff2ea14b5caf26c5
Ruheni Alex <aleruheni@outlook.com>
2018-12-26 18:17:30 +0300

    author, book, bookinstance and genre models created and exported

--- app.js
+++ app.js
@@ -3,16 +3,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Setting up default mongoose connection
+var mongoDB =
+    'mongodb://ruheni:Ruheni99.A@ds253203.mlab.com:53203/local_library';
+
+mongoose.connect(
+    mongoDB,
+    { useNewUrlParser: true }
+);
+
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+
+// Get default connection
+var db = mongoose.connection;
+
+// bind connection error event( to get notification of any errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,18 +46,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
+});
+
+router.get('/cool', (req, res, next) => {
+    res.send("You're so cool");
 });
 
 module.exports = router;
