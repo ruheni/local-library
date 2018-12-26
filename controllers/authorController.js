db2fbc5a917d6621fbf6bf8ee143a6ac24a4e294
Ruheni Alex <aleruheni@outlook.com>
2018-12-26 23:20:02 +0300

    authorController added with dummy handler functions

---
+++ controllers/authorController.js
@@ -0,0 +1,41 @@
+var Author = require('../models/author.js');
+
+// Display list of all Authors.
+exports.author_list = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author List');
+};
+
+// Display detail page for a specific Author
+exports.author_detail = (req, res) => {
+    res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+};
+
+// Display Author create from on GET
+exports.author_create_get = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author create GET');
+};
+
+// Handle Author create on POST
+exports.author_create_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author create POST');
+};
+
+// Handle Author delete form on GET
+exports.author_delete_get = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author delete GET');
+};
+
+// Handle Author delete on POST.
+exports.author_delete_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author delete post');
+};
+
+// Handle Author update form on GET
+exports.author_update_get = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author update GET');
+};
+
+// Handle Author update form on POST
+exports.author_update_post = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author update on POST');
+};
