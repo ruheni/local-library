d9bf3611389a98f7fa8a7c6d9fa03e7ed9fec74e
Ruheni Alex <aleruheni@outlook.com>
2019-01-02 20:27:16 +0300

    Bookinstance_list updated with virtual function for date formatting

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,32 +1,37 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema({
-    book: {
-        type: Schema.Types.ObjectId,
-        ref: 'Book',
-        required: true
-    },
-    imprint: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        required: true,
-        enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'],
-        default: 'Maintenance'
-    },
-    due_back: {
-        type: Date,
-        default: Date.now
-    }
+	book: {
+		type: Schema.Types.ObjectId,
+		ref: 'Book',
+		required: true
+	},
+	imprint: {
+		type: String,
+		required: true
+	},
+	status: {
+		type: String,
+		required: true,
+		enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'],
+		default: 'Maintenance'
+	},
+	due_back: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual('url').get(function() {
-    return `/catalog/bookinstance/${this._id}`;
+	return `/catalog/bookinstance/${this._id}`;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(() => {
+	return moment(this.due_back).format('MMMM Do, YYYY');
 });
 
 //Export model
