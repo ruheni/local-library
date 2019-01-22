eaef99ac63cd30363273b611af8b844aa16c94a4
Ruheni Alex <aleruheni@outlook.com>
2019-01-22 16:07:39 +0300

    eslint devDependency removed
    serve-favicon commented
    
    no changes to:
    author_detail, book_detail, book_instance_detail
    bookController, genreController

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var favicon = require('serve-favicon');
+// var favicon = require("serve-favicon");
 var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
@@ -35,7 +35,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -47,12 +47,12 @@ app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,119 +1,119 @@
-var Book = require('../models/book');
-var Author = require('../models/author');
-var Genre = require('../models/genre');
-var BookInstance = require('../models/bookinstance');
+var Book = require("../models/book");
+var Author = require("../models/author");
+var Genre = require("../models/genre");
+var BookInstance = require("../models/bookinstance");
 
-var async = require('async');
+var async = require("async");
 
 exports.index = (req, res, next) => {
-	async.parallel(
-		{
-			// Pass empty object as match condition to find all
-			// the documents of this collections
-			book_count: callback => {
-				Book.countDocuments({}, callback);
-			},
-			book_instance_count: callback => {
-				BookInstance.countDocuments({}, callback);
-			},
-			book_instance_available_count: callback => {
-				BookInstance.countDocuments({ status: 'Available' }, callback);
-			},
-			author_count: callback => {
-				Author.countDocuments({}, callback);
-			},
-			genre_count: callback => {
-				Genre.countDocuments({}, callback);
-			}
-		},
-		function(err, results) {
-			res.render('index', {
-				title: 'Local Library Home',
-				error: err,
-				data: results
-			});
-		}
-	);
+    async.parallel(
+        {
+            // Pass empty object as match condition to find all
+            // the documents of this collections
+            book_count: callback => {
+                Book.countDocuments({}, callback);
+            },
+            book_instance_count: callback => {
+                BookInstance.countDocuments({}, callback);
+            },
+            book_instance_available_count: callback => {
+                BookInstance.countDocuments({ status: "Available" }, callback);
+            },
+            author_count: callback => {
+                Author.countDocuments({}, callback);
+            },
+            genre_count: callback => {
+                Genre.countDocuments({}, callback);
+            }
+        },
+        function(err, results) {
+            res.render("index", {
+                title: "Local Library Home",
+                error: err,
+                data: results
+            });
+        }
+    );
 };
 
 // Display list of all books
 exports.book_list = (req, res) => {
-	Book.find({}, 'title author')
-		.populate('author')
-		.exec((err, list_books) => {
-			if (err) {
-				return next(err);
-			}
-			console.log(list_books);
-			// Successful, so render
-			res.render('book_list', {
-				title: 'Book List',
-				book_list: list_books
-			});
-			// console.log(book_list);
-		});
+    Book.find({}, "title author")
+        .populate("author")
+        .exec((err, list_books) => {
+            if (err) {
+                return next(err);
+            }
+            console.log(list_books);
+            // Successful, so render
+            res.render("book_list", {
+                title: "Book List",
+                book_list: list_books
+            });
+            // console.log(book_list);
+        });
 };
 
 // Display detail page for a specific book
 exports.book_detail = (req, res, next) => {
-	async.parallel(
-		{
-			book: callback => {
-				Book.findById(req.params.id)
-					.populate('author')
-					.populate('genre')
-					.exec(callback);
-			},
-			book_instance: callback => {
-				BookInstance.find({ book: req.params.id }).exec(callback);
-			}
-		},
-		(err, results) => {
-			if (err) {
-				return next(err);
-			}
-			if (results.book == null) {
-				var err = new Error('Book not found');
-				err.status = 404;
-				return next(err);
-			}
+    async.parallel(
+        {
+            book: callback => {
+                Book.findById(req.params.id)
+                    .populate("author")
+                    .populate("genre")
+                    .exec(callback);
+            },
+            book_instance: callback => {
+                BookInstance.find({ book: req.params.id }).exec(callback);
+            }
+        },
+        (err, results) => {
+            if (err) {
+                return next(err);
+            }
+            if (results.book == null) {
+                var err = new Error("Book not found");
+                err.status = 404;
+                return next(err);
+            }
 
-			//Successful, therefore, render
-			res.render('book_detail', {
-				title: 'Title',
-				book: results.book,
-				book_instances: results.book_instance
-			});
-		}
-	);
+            //Successful, therefore, render
+            res.render("book_detail", {
+                title: "Title",
+                book: results.book,
+                book_instances: results.book_instance
+            });
+        }
+    );
 };
 
 // Display book create form on GET
 exports.book_create_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: Book create GET');
+    res.send("NOT IMPLEMENTED: Book create GET");
 };
 
 // Handle book create on POST
 exports.book_create_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: Book create POST');
+    res.send("NOT IMPLEMENTED: Book create POST");
 };
 
 // Display book delete form on GET
 exports.book_delete_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: Book delete GET');
+    res.send("NOT IMPLEMENTED: Book delete GET");
 };
 
 // Handle book delete on POST
 exports.book_delete_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: Book delete POST');
+    res.send("NOT IMPLEMENTED: Book delete POST");
 };
 
 // Display book update form on GET
 exports.book_update_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: Book update GET');
+    res.send("NOT IMPLEMENTED: Book update GET");
 };
 
 // Handle book update on POST
 exports.book_update_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: Book update POST');
+    res.send("NOT IMPLEMENTED: Book update POST");
 };
